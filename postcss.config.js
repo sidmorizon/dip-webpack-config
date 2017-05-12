25e3479ea956b82e0156d9e11800b97baca15a74
zuozhuo <zuozhuo@xmzhen.com>
2017-05-12 16:11:46 +0800

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,11 @@
 /**
  * Created by zuozhuo on 2017/5/10.
  */
-'use strict'
 const postCssPlugins = require('./lib/postCssPlugins');
 
 module.exports = {
     sourceMap: true, // 只能设置 true | inline 不能设置false
     // TODO 将post-css的plugin加入到这里（autoPrefix、pxtorem等）
-    plugins: postCssPlugins // 这里不能使用plugin属性，请在后面的 postcss 属性配置
+    plugins: postCssPlugins, // 这里不能使用plugin属性，请在后面的 postcss 属性配置
     // parser: 'postcss-scss' // 如果需要使用 precss，需要设置这个parser
 };
